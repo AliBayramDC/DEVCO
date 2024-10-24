1c9ebccdbd3d4759da0f7f06bce3dcfb42273bfd
AliBayramDC <121183289+AliBayramDC@users.noreply.github.com>
2024-10-24 19:40:52 +0400

    Update email.js

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,34 +1,30 @@
 const nodemailer = require("nodemailer");
-const fs = require("fs");
 const path = require("path");
 
-// Configure Nodemailer transporter
+// Configure Nodemailer transporter for GreenGeeks email
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "ams202.greengeeks.net",  // GreenGeeks outgoing hostname
+  port: 465,  // Port for SSL
+  secure: true,  // Use SSL for port 465
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER,  // Your full email address (info@devco.az)
+    pass: process.env.EMAIL_PASS,  // The password for this email
   },
 });
 
-// Function to send an email with the generated PDF as an attachment and embed images
+// Function to send an email with the generated PDF as an attachment
 exports.sendEmail = (to, pdfPath, name) => {
   const mailOptions = {
-    from: process.env.EMAIL_USER,
+    from: process.env.EMAIL_USER,  // Your email address (info@devco.az)
     to,
     subject: "DEVCO-dan Zərərsizlik nöqtəsi (BEP) hesablanması.",
     html: `
       <p style="font-size: 19px;">Hörmətli <strong>${name}</strong>,</p>
       <p style="font-size: 19px;">Zərərsizlik nöqtəsi haqqında izahlı məlumatı özündə əks etdirən <strong>PDF sənədini</strong> bu e-poçta əlavə etdim, lakin getməzdən əvvəl sizə sizin və ya biznesiniz üçün faydalı ola biləcək bəzi əlavə xidmətlərlə tanış etmək istərdim.</p>
-
-      <!-- Make the image clickable with a link to your website -->
       <p><a href="https://www.devco.az" target="_blank"><img src="cid:serviceImage1" alt="Service Image 1" style="width: 100%; max-width: 900px; height: auto;"></a></p>
-
       <p style="font-size: 19px;">Bu xidmətlərin sizin məqsədlərinizə necə dəyər verə biləcəyini müzakirə etmək üçün zəng və ya video görüş təyin etməkdən məmnun olaram.</p>
-      
       <p style="font-size: 19px;">Hörmətlə,<br><strong>Arzu Bayramov</strong><br><strong>Founder and Director</strong><br><a href="https://www.devco.az" target="_blank" style="color: #0000EE; text-decoration: none; font-size: 19px; font-weight: bold;">DEVCO Training and Consulting</a><br>Tel. +994702240677</p>
     `,
-
     attachments: [
       {
         filename: "breakeven-report.pdf",
@@ -36,7 +32,7 @@ exports.sendEmail = (to, pdfPath, name) => {
       },
       {
         filename: "serviceImage1.png",
-        path: path.join(__dirname, "../images/Blue and White School Trifold Brochure.png"), // Updated image
+        path: path.join(__dirname, "../images/Blue and White School Trifold Brochure.png"), // Example image
         cid: "serviceImage1",
       },
     ],
